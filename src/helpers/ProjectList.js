aff4419eaa681601ca727eb7d81311e4312c0a22
Eric Webber <ewebber283@gmail.com>
2022-10-01 18:10:20 -0500

    added project list and images

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,42 @@
+import bSafe from '../assets/hero.png';
+import pythonTech from '../assets/pythonTechNews.png'
+import reactiveCooking from '../assets/reactiveCooking.png'
+import photoPort from '../assets/photoPort.png'
+
+export const ProjectList = [
+    {
+        name: 'B.Safe',
+        image: bSafe,
+        skills: 'CSS, HTML, JS',
+        website: 'https://ewebber283.github.io/B-Safe/',
+        github: 'https://github.com/ewebber283/B-Safe'
+    },
+    {
+        name: 'Just Tech News',
+        image: pythonTech,
+        skills: 'Python',
+        website: 'https://python-tech-news-28.herokuapp.com/',
+        github: 'https://github.com/ewebber283/python-newsfeed'
+    },
+    {
+        name: 'Reactive Cooking',
+        image: reactiveCooking,
+        skills: 'React',
+        website: 'https://reactive-cooking.netlify.app/',
+        github: 'https://github.com/ewebber283/reactive-cooking'
+    },
+    {
+        name: 'Photo Port',
+        image: photoPort,
+        skills: 'React',
+        website: 'https://ewebber283.github.io/photo-port/',
+        github: 'https://github.com/ewebber283/photo-port'
+    },
+    {
+        name: 'B.Safe',
+        image: bSafe,
+        skills: 'CSS, HTML, JS',
+        website: 'https://ewebber283.github.io/B-Safe/',
+        github: 'https://github.com/ewebber283/B-Safe'
+    },
+]
